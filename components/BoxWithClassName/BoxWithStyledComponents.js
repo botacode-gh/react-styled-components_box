71f056b53d7389e7110243aec7520b00d2f66b39
Xaver Schüll <botacode@gmail.com>
2023-03-30 12:37:45 +0200

    Added boxes with styled components

---
+++ components/BoxWithClassName/BoxWithStyledComponents.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function BoxWithStyledComponents({ isBlack }) {
+  return <StyledBox isBlack={isBlack} />;
+}
+
+const StyledBox = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 2rem;
+
+  background-color: ${({ isBlack }) => (isBlack ? "black" : "green")};
+
+  &:hover {
+    background-color: red;
+  }
+`;
